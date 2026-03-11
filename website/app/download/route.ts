d988997a757599a6cb68c73b6965f3a6f417f1ea
junyuw2289-svg <junyuw2289@gmail.com>
2026-03-11 12:45:40 -0700

    Point download route at latest GitHub release

--- website/app/download/route.ts
+++ website/app/download/route.ts
@@ -1,8 +1,8 @@
 import { redirect } from "next/navigation";
 
 const FALLBACK_DOWNLOAD_URL =
-  "https://github.com/junyuw2289-svg/voicepaste/releases/latest/download/VoicePaste.dmg";
+  "https://github.com/Jaredw2289-svg/voicepaste/releases/latest/download/VoicePaste.dmg";
 
 export async function GET() {
-  redirect(process.env.VOICEPASTE_DMG_URL || FALLBACK_DOWNLOAD_URL);
+  redirect(FALLBACK_DOWNLOAD_URL);
 }
